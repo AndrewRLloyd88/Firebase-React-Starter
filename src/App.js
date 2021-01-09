71d0378cb123571e7af9d0b284010db88afe2425
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-01-09 10:26:58 -0800

    proof of concept app with Read and Write in firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import { db } from './firebase';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //realtime listener that will map our posts from firebase
+    db.collection('posts').onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+      console.log(posts);
+    });
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    db.collection('posts').add({
+      name: 'Any Name',
+      description: 'this is a test',
+      message: input,
+      photoURL: '',
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button onClick={sendPost} type="submit">
+          Send
+        </button>
+      </form>
+
+      {posts.map(({ id, data: { name, description, message, photoURL } }) => {
+        return (
+          <>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <p>{message}</p>
+          </>
+        );
+      })}
     </div>
   );
 }
